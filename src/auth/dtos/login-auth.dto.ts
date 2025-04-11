f61f2527ae7036183482534a5952a499e0d95dd0
leonx04 <jungnguyen2108@gmail.com>
2025-04-11 16:21:06 +0700

    feat(auth): refactor LoginAuthDto and RegisterAuthDto to extend CreateUserDTO for cleaner code

--- src/auth/dtos/login-auth.dto.ts
+++ src/auth/dtos/login-auth.dto.ts
@@ -1,12 +1,6 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { PickType } from '@nestjs/swagger';
+import { CreateUserDTO } from 'src/users/dtos/create-user.dto';
 
-export class LoginAuthDto {
-  @IsEmail()
-  @ApiProperty({ description: 'User email address' })
-  email: string;
-
-  @IsNotEmpty()
-  @ApiProperty({ description: 'User password' })
-  password: string;
-}
+export class LoginAuthDto extends PickType(CreateUserDTO, ['email', 'password'] as const) { }
+// Chỉ cần kế thừa PickType và truyền vào CreateUserDTO và các thuộc tính muốn lấy
+// Chỉ lấy email và password từ CreateUserDTO
--- src/auth/dtos/register-auth.dto.ts
+++ src/auth/dtos/register-auth.dto.ts
@@ -2,3 +2,5 @@ import { OmitType } from '@nestjs/swagger';
 import { CreateUserDTO } from 'src/users/dtos/create-user.dto';
 
 export class RegisterAuthDto extends OmitType(CreateUserDTO, ['role'] as const) { }
+// Trừ role ra khỏi CreateUserDTO
+// Chỉ cần kế thừa OmitType và truyền vào CreateUserDTO và các thuộc tính muốn loại bỏ
